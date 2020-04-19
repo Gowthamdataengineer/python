46bb0a6da93eeb684e4102ad064f027d249c14c1
atozknowledge <atozknowledge.com@gmail.com>
2020-04-19 23:04:48 +0530

    Create face_detect.py

---
+++ face_detect.py
@@ -0,0 +1,29 @@
+#www.atozknowledge.com
+
+import cv2
+
+imagePath = "/home/gowtham/test.png"
+cascPath = "/home/gowtham/haarcascade_frontalface_default.xml"
+
+faceCascade = cv2.CascadeClassifier(cascPath)
+
+image = cv2.imread(imagePath)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+faces = faceCascade.detectMultiScale(
+    gray,
+    scaleFactor=1.1,
+    minNeighbors=5,
+    minSize=(30, 30),
+    flags = cv2.CASCADE_SCALE_IMAGE
+)
+
+
+print ("Found {0} faces!".format(len(faces)))
+
+# Draw a rectangle around the faces
+for (x, y, w, h) in faces:
+    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+cv2.imshow("Faces found", image)
+cv2.waitKey(0)
